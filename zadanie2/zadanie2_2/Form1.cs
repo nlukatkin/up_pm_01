3dd037d97f300f2f0fe59bac21a17d8ff3fb3a34
MARK <klukatkin02@bk.ru>
2021-12-04 11:05:36 +0300

    Выполнил упражнение 2 задания 2.

---
+++ zadanie2/zadanie2_2/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WinAsynchDelegate {
+    public partial class Form1 : Form {
+        private delegate void TimeConsumingMethodDelegate(int seconds);
+        public delegate void SetProgressDelegate(int val);
+
+        private bool Cancel { get; set; } = false;
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) {
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e) {
+            if (!char.IsDigit(e.KeyChar)) {
+                e.Handled = true;
+                MessageBox.Show("Поле должно содержать цифры");
+            }
+        }
+
+        private void TimeConsumingMethod(int seconds)
+        {
+            for (int j = 1; j <= seconds; j++) {
+                if (Cancel) {
+                    break;
+                }
+
+                SetProgress((int)(j * 100) / seconds);
+                System.Threading.Thread.Sleep(1000);
+            }
+
+            if (Cancel) {
+                MessageBox.Show("Cancelled");
+                Cancel = false;
+            }
+            else {
+                MessageBox.Show("Complete");
+            }
+        }
+
+        public void SetProgress(int val) {
+            if (progressBar1.InvokeRequired) {
+                SetProgressDelegate del = new SetProgressDelegate(SetProgress);
+                this.Invoke(del, new object[] { val });
+            }
+        else {
+                progressBar1.Value = val;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            TimeConsumingMethodDelegate del = new TimeConsumingMethodDelegate(TimeConsumingMethod);
+            del.BeginInvoke(int.Parse(textBox1.Text), null, null);
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            Cancel = true;
+        }
+    }
+}
---
+++ zadanie2/zadanie2_2/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("WinAsynchDelegate")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WinAsynchDelegate")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("ac7b1b8c-5828-41a2-b3c3-5be06c8ea0d5")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
