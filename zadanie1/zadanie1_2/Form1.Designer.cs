66950c4742e443cb3b304debb26be4eb26a00120
MARK <klukatkin02@bk.ru>
2021-12-04 10:55:03 +0300

    Создал второе упражнение первого задания.

---
+++ zadanie1/zadanie1_2/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace WinAnim
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAW_BLEND = new System.Windows.Forms.Button();
+            this.btnHOR_AW_SLIDE = new System.Windows.Forms.Button();
+            this.btnCenter_AW_SLIDE = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAW_BLEND
+            // 
+            this.btnAW_BLEND.Location = new System.Drawing.Point(30, 62);
+            this.btnAW_BLEND.Name = "btnAW_BLEND";
+            this.btnAW_BLEND.Size = new System.Drawing.Size(232, 23);
+            this.btnAW_BLEND.TabIndex = 0;
+            this.btnAW_BLEND.Text = "Проявление";
+            this.btnAW_BLEND.UseVisualStyleBackColor = true;
+            this.btnAW_BLEND.Click += new System.EventHandler(this.btnAW_BLEND_CLICK);
+            // 
+            // btnHOR_AW_SLIDE
+            // 
+            this.btnHOR_AW_SLIDE.Location = new System.Drawing.Point(30, 118);
+            this.btnHOR_AW_SLIDE.Name = "btnHOR_AW_SLIDE";
+            this.btnHOR_AW_SLIDE.Size = new System.Drawing.Size(232, 23);
+            this.btnHOR_AW_SLIDE.TabIndex = 1;
+            this.btnHOR_AW_SLIDE.Text = "Горизонтальное появление";
+            this.btnHOR_AW_SLIDE.UseVisualStyleBackColor = true;
+            this.btnHOR_AW_SLIDE.Click += new System.EventHandler(this.btnHOR_AW_SLIDE_Click);
+            // 
+            // btnCenter_AW_SLIDE
+            // 
+            this.btnCenter_AW_SLIDE.Location = new System.Drawing.Point(30, 182);
+            this.btnCenter_AW_SLIDE.Name = "btnCenter_AW_SLIDE";
+            this.btnCenter_AW_SLIDE.Size = new System.Drawing.Size(232, 23);
+            this.btnCenter_AW_SLIDE.TabIndex = 2;
+            this.btnCenter_AW_SLIDE.Text = "Проявление из центра";
+            this.btnCenter_AW_SLIDE.UseVisualStyleBackColor = true;
+            this.btnCenter_AW_SLIDE.Click += new System.EventHandler(this.btnCenter_AW_SLIDE_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCenter_AW_SLIDE);
+            this.Controls.Add(this.btnHOR_AW_SLIDE);
+            this.Controls.Add(this.btnAW_BLEND);
+            this.Name = "Form1";
+            this.Text = "Анимация формы";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAW_BLEND;
+        private System.Windows.Forms.Button btnHOR_AW_SLIDE;
+        private System.Windows.Forms.Button btnCenter_AW_SLIDE;
+    }
+}
+
---
+++ zadanie1/zadanie1_2/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace WinAnim {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        public class WinAPIClass {
+            #region Анимация окна
+            [Flags]
+            public enum AnimateWindowFlags : int {
+                AW_HOR_POSITIVE = 1,
+                AW_HOR_NEGATIVE = 2,
+                AW_VER_POSITIVE = 4,
+                AW_VER_NEGATive = 8,
+                AW_CENTER = 16,
+                AW_HIDE = 65536,
+                AW_ACTIVATE = 131072,
+                AW_SLIDE = 262144,
+                AW_BLEND = 524288,
+            };
+            [DllImportAttribute("user32.dll", EntryPoint = "AnimateWindow", SetLastError = true)]
+            public static extern bool AnimateWindow(IntPtr hwnd, int dwTime, int dwFlags);
+            public static bool AnimateWindow(Control ctrl, int dwTime, AnimateWindowFlags Flags) {
+                return AnimateWindow(ctrl.Handle, dwTime, (int)Flags);
+                #endregion
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+
+        }
+
+        private void btnAW_BLEND_CLICK(object sender, EventArgs e) {
+            this.Hide();
+            WinAPIClass.AnimateWindow(this, 3000, WinAPIClass.AnimateWindowFlags.AW_ACTIVATE | WinAPIClass.AnimateWindowFlags.AW_BLEND);
+            this.btnAW_BLEND.Invalidate();
+            this.btnHOR_AW_SLIDE.Invalidate();
+            this.btnCenter_AW_SLIDE.Invalidate();
+        }
+
+        private void btnHOR_AW_SLIDE_Click(object sender, EventArgs e) {
+            this.Hide();
+            WinAPIClass.AnimateWindow(this, 3000, WinAPIClass.AnimateWindowFlags.AW_HOR_POSITIVE | WinAPIClass.AnimateWindowFlags.AW_SLIDE);
+            this.btnAW_BLEND.Invalidate();
+            this.btnHOR_AW_SLIDE.Invalidate();
+            this.btnCenter_AW_SLIDE.Invalidate();
+        }
+
+        private void btnCenter_AW_SLIDE_Click(object sender, EventArgs e) {
+            this.Hide();
+            WinAPIClass.AnimateWindow(this, 3000, WinAPIClass.AnimateWindowFlags.AW_CENTER | WinAPIClass.AnimateWindowFlags.AW_SLIDE);
+            this.btnAW_BLEND.Invalidate();
+            this.btnHOR_AW_SLIDE.Invalidate();
+            this.btnCenter_AW_SLIDE.Invalidate();
+        }
+    }
+}
