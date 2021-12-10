b69a06354a39b327813034b0cbaa1b380909c687
MARK <klukatkin02@bk.ru>
2021-12-10 14:40:53 +0300

    Создал 4 упражнение 4 задания.

---
+++ zadanie4/zadanie4_4/WinDataBinding/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace WinDataBinding
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rbProductDataSet1 = new WinDataBinding.RBProductDataSet();
+            this.товарыTableAdapter1 = new WinDataBinding.RBProductDataSetTableAdapters.ТоварыTableAdapter();
+            this.FamtextBox = new System.Windows.Forms.TextBox();
+            this.NametextBox = new System.Windows.Forms.TextBox();
+            this.SectiontextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Previousbutton = new System.Windows.Forms.Button();
+            this.Nextbutton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.rbProductDataSet1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rbProductDataSet1
+            // 
+            this.rbProductDataSet1.DataSetName = "RBProductDataSet";
+            this.rbProductDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // товарыTableAdapter1
+            // 
+            this.товарыTableAdapter1.ClearBeforeFill = true;
+            // 
+            // FamtextBox
+            // 
+            this.FamtextBox.Location = new System.Drawing.Point(121, 36);
+            this.FamtextBox.Name = "FamtextBox";
+            this.FamtextBox.Size = new System.Drawing.Size(100, 20);
+            this.FamtextBox.TabIndex = 0;
+            // 
+            // NametextBox
+            // 
+            this.NametextBox.Location = new System.Drawing.Point(121, 87);
+            this.NametextBox.Name = "NametextBox";
+            this.NametextBox.Size = new System.Drawing.Size(100, 20);
+            this.NametextBox.TabIndex = 1;
+            // 
+            // SectiontextBox
+            // 
+            this.SectiontextBox.Location = new System.Drawing.Point(121, 140);
+            this.SectiontextBox.Name = "SectiontextBox";
+            this.SectiontextBox.Size = new System.Drawing.Size(100, 20);
+            this.SectiontextBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Наименование";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Цена";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Код поставщика";
+            // 
+            // Previousbutton
+            // 
+            this.Previousbutton.Location = new System.Drawing.Point(28, 198);
+            this.Previousbutton.Name = "Previousbutton";
+            this.Previousbutton.Size = new System.Drawing.Size(75, 23);
+            this.Previousbutton.TabIndex = 7;
+            this.Previousbutton.Text = "Previous";
+            this.Previousbutton.UseVisualStyleBackColor = true;
+            this.Previousbutton.Click += new System.EventHandler(this.Previousbutton_Click);
+            // 
+            // Nextbutton
+            // 
+            this.Nextbutton.Location = new System.Drawing.Point(133, 198);
+            this.Nextbutton.Name = "Nextbutton";
+            this.Nextbutton.Size = new System.Drawing.Size(75, 23);
+            this.Nextbutton.TabIndex = 8;
+            this.Nextbutton.Text = "Next";
+            this.Nextbutton.UseVisualStyleBackColor = true;
+            this.Nextbutton.Click += new System.EventHandler(this.Nextbutton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.Nextbutton);
+            this.Controls.Add(this.Previousbutton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SectiontextBox);
+            this.Controls.Add(this.NametextBox);
+            this.Controls.Add(this.FamtextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.rbProductDataSet1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private RBProductDataSet rbProductDataSet1;
+        private RBProductDataSetTableAdapters.ТоварыTableAdapter товарыTableAdapter1;
+        private System.Windows.Forms.TextBox FamtextBox;
+        private System.Windows.Forms.TextBox NametextBox;
+        private System.Windows.Forms.TextBox SectiontextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button Previousbutton;
+        private System.Windows.Forms.Button Nextbutton;
+    }
+}
+
---
+++ zadanie4/zadanie4_4/WinDataBinding/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinDataBinding
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private BindingSource sotrBindingSourse;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            товарыTableAdapter1.Fill(rbProductDataSet1.Товары);
+            sotrBindingSourse = new BindingSource(rbProductDataSet1, "Товары");
+            FamtextBox.DataBindings.Add("Text", sotrBindingSourse, "Наименование");
+            NametextBox.DataBindings.Add("Text", sotrBindingSourse, "Цена, $");
+            SectiontextBox.DataBindings.Add("Text", sotrBindingSourse, "Код поставщика");
+        }
+
+        private void Previousbutton_Click(object sender, EventArgs e)
+        {
+            sotrBindingSourse.MovePrevious();
+        }
+
+        private void Nextbutton_Click(object sender, EventArgs e)
+        {
+            sotrBindingSourse.MoveNext();
+        }
+    }
+}
