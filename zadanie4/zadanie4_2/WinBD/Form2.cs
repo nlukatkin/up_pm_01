3f30be925f29ec5b235f5aca97d223b0157fa3ef
MARK <klukatkin02@bk.ru>
2021-12-10 14:34:40 +0300

    Создал 2 упражнение 4 задания.

---
+++ zadanie4/zadanie4_2/WinBD/Form2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinBD
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            oleDbDataAdapter1.Fill(dataSet11.Поставщики);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            oleDbDataAdapter1.Update(dataSet11);
+        }
+    }
+}
